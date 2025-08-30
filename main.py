6de401d7217236742e7fbeee2a71b163043a2424
Queena <148352740+scoopydoooKing@users.noreply.github.com>
2025-08-30 22:27:24 +0800

    code to clean data and read file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from vector import retriever
+import pandas as pd
+# from vector import retriever
 
 model = OllamaLLM(model = "llama3.2")
 
@@ -39,12 +40,63 @@ Example Output:
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
+
+def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+  df = df.drop_duplicates()
+  df = df.dropna()
+  df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
+  df['rating'] = df['rating'].clip(lower=0, upper=5)
+  df = df.dropna(subset=['rating'])
+  df['rating'] = df['rating'].astype(int)
+  
+  df = df[df['text'].astype(str).str.strip() != ""]
+  
+  return df 
+
+def read_file(file_name: str) -> pd.DataFrame:
+  file_format = file_name.split(".")[-1].lower()
+
+  if file_format == "csv":
+    df = pd.read_csv(file_name)
+  elif file_format in ["xls", "xlsx", "xlsm", "xlsb", "ods"]:
+    df = pd.read_excel(file_name)
+  elif file_format == "json":
+    df = pd.read_json(file_name)
+  elif file_format == "xml":
+    df = pd.read_xml(file_name)
+  else:
+    raise ValueError(f"Unsupported file format: {file_format}")
+  
+  return df
+  
 while True:
-    print("\n\n-----------------------")
-    user_input = input("Input your review (q to quit): ") # should include location information to evaluate policy 2
-    print("\n\n")
-    if user_input == "q":
-        break
-    # reviews = retriever.invoke(user_input)
-    result = chain.invoke({"reference": [], "review": user_input}) # update reference: reviews for RAG
-    print(result)
+  user_input = ''
+  print("\n\n")
+  mode = input("Do you want to check reviews individually or from a file? (type 'individual', 'file', or 'quit'): ").strip().lower()
+
+  if mode == "quit":
+      print("Exiting program. Goodbye!")
+      
+  elif mode == "individual":
+      review = input("Enter your review (or type 'quit' to exit): ").strip()
+      user_input = review
+      if review.lower() == "quit":
+          print("Exiting program. Goodbye!")
+          break
+
+  elif mode == "file":
+      file_name = input("Enter the file name (with extension, e.g. data.csv) or type 'quit' to exit: ").strip()
+      if file_name.lower() == "quit":
+          print("Exiting program. Goodbye!")
+          break
+      else: 
+        clean_df = clean_dataframe(read_file(file_name))
+        if len(clean_df['text']) > 0: 
+          # get top 5 of the comment instead of all, can change later
+          user_input = clean_df['text'].head(5)
+
+  print("\n\n")
+  # reviews = retriever.invoke(user_input)
+  result = chain.invoke({"reference": [], "review": user_input}) # update reference: reviews for RAG
+  print(result)
+
